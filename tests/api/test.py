00db5b47c1f70a02932146262341c1b950654109
Fernanda Serra <fernandafmst@decea.mil.br>
2024-11-28 14:59:37 -0300

    added fluent log forwarding

--- tests/api/test.py
+++ tests/api/test.py
@@ -1,11 +1,10 @@
 from os import environ
 import logging
 
-logger = logging.getLogger("default")
+logger = logging.getLogger("http_logger")
 
 
 # TC example
 def test_api_url() -> None:
     api_url = environ.get("USSP_URL")
-    logger.debug("constructing session thing")
     assert api_url == "https://localhost:8000"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,37 @@
 import os
+from fluent import handler
+import logging
+import pytest
+from dotenv import load_dotenv
+
+custom_log_format = {
+    "host": "%(hostname)s",
+    "where": "%(module)s.%(funcName)s",
+    "type": "%(levelname)s",
+    "stack_trace": "%(exc_text)s",
+}
+
+fluentd_tag = "pytest.logs"
+
+@pytest.fixture(autouse=True)
+def load_env():
+   load_dotenv() 
+
 
 def pytest_logger_config(logger_config):
-   logger_config.add_loggers(['http_logger', 'default'], stdout_level='debug')
-   logger_config.set_log_option_default('default')
+    fluentd_host = os.environ.get("FLUENT_HOST_URL")
+    fluentd_port = 24224
+      
+    logger_config.add_loggers(["http_logger", "default"], stdout_level="debug")
+    logger_config.set_log_option_default("default")
 
-def pytest_logger_logdirlink():
-   return os.path.join(os.path.dirname(__file__), 'logs')
+    fluent_handler = handler.FluentHandler(
+        fluentd_tag, host=str(fluentd_host), port=fluentd_port
+    )
+    log_formatter = handler.FluentRecordFormatter(custom_log_format)
+    fluent_handler.setFormatter(log_formatter)
+
+    logger = logging.getLogger("http_logger")
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(fluent_handler)
